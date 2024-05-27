3e5426a891f674d347c23b1581e42fd311d98b63
Alexander loperena <alexandersauna05@gmail.com>
2024-05-27 12:42:06 -0500

    se agregaron correpciones al codigo fuente del proyecto

---
+++ proyecto_canino/slider.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll(".slides img");
+    let currentSlide = 0;
+  
+    function showSlide(index) {
+      slides.forEach((slide, i) => {
+        if (i === index) {
+          slide.style.display = "block";
+        } else {
+          slide.style.display = "none";
+        }
+      });
+    }
+  
+    function nextSlide() {
+      currentSlide = (currentSlide + 1) % slides.length;
+      showSlide(currentSlide);
+    }
+    setInterval(nextSlide, 4000);
+  });
+  
